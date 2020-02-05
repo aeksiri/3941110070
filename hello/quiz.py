ddd066a4f8a1312587688524908f63a380925479
Akkharaphong EKSIRI <akkharaphong.e@bu.ac.th>
2020-02-05 14:49:58 +0700

    blah blah blah

--- hello/quiz.py
+++ hello/quiz.py
@@ -1,4 +1,13 @@
 msg = "My name is Akkharaphong"
 msg2 = "I'm from Thailand"
 msg3 = "jkajdfkjd;afjkasdjfkl;xxxxxxxxxxxxx"
+msg = "My name is Akkharaphong"
+msg2 = "I'm from Thailand"
+msg3 = "jkajdfkjd;afjkasdjfkl;xxxxxxxxxxxxx"
+msg = "My name is Akkharaphong"
+msg2 = "I'm from Thailand"
+msg3 = "jkajdfkjd;afjkasdjfkl;xxxxxxxxxxxxx"
+msg = "My name is Akkharaphong"
+msg2 = "I'm from Thailand"
+msg3 = "jkajdfkjd;afjkasdjfkl;xxxxxxxxxxxxx"
 print(msg+msg2+msg3)
